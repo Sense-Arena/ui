a2644e2e1e0dfd2139d284be7712fe5bace7a48a
Konstantin <konstantin.miheev@sensearena.com>
2022-09-05 12:50:18 +0200

    tuned config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { peerDependencies } from './package.json';
 
 export default defineConfig({
   plugins: [
     vanillaExtractPlugin(),
-    react(),
+    react({
+      jsxRuntime: 'classic',
+    }),
     dts({
       outputDir: path.resolve(__dirname, 'dist/types'),
       exclude: ['**/stories/**'],
@@ -18,32 +21,11 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@sa-ui',
-      formats: ['es', 'umd'],
+      formats: ['es', 'cjs'],
       fileName: format => `@sa-ui.${format}.js`,
     },
     rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        '@mui/material',
-        '@react-spring/web',
-        '@vanilla-extract/css',
-        '@vanilla-extract/recipes',
-        '@emotion/react',
-        '@emotion/styled',
-      ],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@mui/material': 'MUI',
-          '@react-spring/web': 'SpringWeb',
-          '@vanilla-extract/css': 'VanillaCss',
-          '@vanilla-extract/recipes': 'VanillaRecipes',
-          '@emotion/react': 'EmoReact',
-          '@emotion/styled': 'Emo',
-        },
-      },
+      external: [...Object.keys(peerDependencies)],
     },
   },
 });
