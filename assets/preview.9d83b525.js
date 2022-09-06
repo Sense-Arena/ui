055686f829980183eef9b981456e043ee59fee37
woophi <attendentofsky@gmail.com>
2022-09-06 10:27:10 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@e17c88e4a14f907e8784ba8c6550ec0fcc79f695 🚀

---
+++ assets/preview.9d83b525.js
@@ -0,0 +1,2 @@
+import{j as r}from"./jsx-runtime.881626c7.js";/* empty css                             */import"./iframe.1f0da806.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
+//# sourceMappingURL=preview.9d83b525.js.map
