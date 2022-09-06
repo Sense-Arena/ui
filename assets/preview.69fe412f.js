18ebcb339ecd60ea4e9ef319486a1f0aa9e8ba6b
Konstantin <konstantin.miheev@sensearena.com>
2022-09-06 11:54:10 +0200

    Updates

---
+++ assets/preview.69fe412f.js
@@ -0,0 +1,2 @@
+import{j as r}from"./jsx-runtime.ce123faa.js";/* empty css                             */import"./iframe.db4177be.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
+//# sourceMappingURL=preview.69fe412f.js.map
--- assets/preview.b04e60a0.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./jsx-runtime.60eb5b61.js";/* empty css                             */import"./iframe.38f86a0c.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
-//# sourceMappingURL=preview.b04e60a0.js.map
