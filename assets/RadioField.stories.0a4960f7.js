17fa1d9a0ee93746dbe8671c75fea7c444c9ecfa
Denior1 <36446798+Denior1@users.noreply.github.com>
2022-09-07 10:27:47 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@2e5654a711f63171a3f8bed4f1f532afddaef493 🚀

---
+++ assets/RadioField.stories.0a4960f7.js
@@ -0,0 +1,29 @@
+import{R as o}from"./TextField.4174d4c3.js";import{a as t,F as a,j as n}from"./jsx-runtime.28c003ac.js";import"./vanilla-extract-recipes-createRuntimeFn.esm.89755c4d.js";import"./Paragraph.991a2927.js";/* empty css                             */import"./styled.b2b57946.js";import"./TransitionGroupContext.70bdf267.js";import"./iframe.82d53202.js";const R={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { RadioField } from '../../components/inputs';
+
+export default {
+  title: 'UI/Inputs/RadioField',
+  component: RadioField,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
+} as ComponentMeta<typeof RadioField>;
+
+const Template: ComponentStory<typeof RadioField> = args => (
+  <>
+    <RadioField {...args} />
+    <RadioField {...args} />
+  </>
+);
+
+export const Base = Template.bind({});
+Base.args = {
+  label: 'First Name',
+  radioProps: {
+    name: 'first_name',
+  },
+};
+`,locationsMap:{base:{startLoc:{col:52,line:14},endLoc:{col:1,line:19},startBody:{col:52,line:14},endBody:{col:1,line:19}}}}},title:"UI/Inputs/RadioField",component:o,argTypes:{disabled:{type:"boolean"}}},i=e=>t(a,{children:[n(o,{...e}),n(o,{...e})]}),r=i.bind({});r.args={label:"First Name",radioProps:{name:"first_name"}};const b=["Base"];export{r as Base,b as __namedExportsOrder,R as default};
+//# sourceMappingURL=RadioField.stories.0a4960f7.js.map
