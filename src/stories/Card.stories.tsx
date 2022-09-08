e40d2fd7ed7563e72441fbb32d6dd211192c1f05
Denis Savelev <denis.savelev@sensearena.com>
2022-09-08 14:58:27 +0400

    <></> to null, if title is not string render without Heading

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -50,6 +50,27 @@ Base.args = {
   ),
 };
 
+export const TitleIsReactNode = Template.bind({});
+TitleIsReactNode.args = {
+  title: <Paragraph variant="note">Title</Paragraph>,
+  action: (
+    <Button minWidth={true} mode="square" color="outline_secondary">
+      Action
+    </Button>
+  ),
+  children: (
+    <>
+      <Paragraph variant="caption">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Paragraph>
+      <Button style={{ marginTop: 12 }}>Test</Button>
+    </>
+  ),
+};
+
 export const WithoutTitleAndAction = Template.bind({});
 WithoutTitleAndAction.args = {
   title: undefined,
