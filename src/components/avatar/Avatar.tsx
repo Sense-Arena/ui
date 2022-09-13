a6858ee9ce2c4a1b2fecdcede427f201cf96d03a
Denis Savelev <denis.savelev@sensearena.com>
2022-09-13 18:35:15 +0400

    remove dicebear, add className prop

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,16 +1,19 @@
 import { avatarStyles } from './avatar.css';
+import { clsx } from '../../utils';
 
 type Props = {
-  src?: string | null;
+  src: string;
   bg?: 'white';
   name?: string;
   size?: 's' | 'm';
+  alt?: string;
+  className?: string;
 };
 
-export const Avatar = ({ src, bg, name, size }: Props) => {
+export const Avatar = ({ src, bg, size, alt, className }: Props) => {
   return (
     <div className={avatarStyles.container({ bg, size })}>
-      <img src={src ?? `https://avatars.dicebear.com/api/initials/${name}.svg`} className={avatarStyles.img} />
+      <img src={src} alt={alt} className={clsx(avatarStyles.img, className)} />
     </div>
   );
 };
--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -8,26 +8,13 @@ export default {
 
 const Template: ComponentStory<typeof Avatar> = args => <Avatar {...args} />;
 
-export const DefaultSize = Template.bind({});
-DefaultSize.args = {
+export const Base = Template.bind({});
+Base.args = {
   src: 'https://sensearena.ams3.cdn.digitaloceanspaces.com/other/1d680f64-b91d-4e66-af8c-edb5541e6e04/a8013994-c771-4398-ae9f-75e6c92bc694.png',
-  name: 'Test Image',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 's',
   src: 'https://sensearena.ams3.cdn.digitaloceanspaces.com/other/1d680f64-b91d-4e66-af8c-edb5541e6e04/a8013994-c771-4398-ae9f-75e6c92bc694.png',
-  name: 'Test Image',
-};
-
-export const WithoutImage = Template.bind({});
-WithoutImage.args = {
-  name: 'Test Image',
-};
-
-export const WithoutImageSmall = Template.bind({});
-WithoutImageSmall.args = {
-  size: 's',
-  name: 'Test Image',
 };
