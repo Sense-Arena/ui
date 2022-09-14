ad85737cad907efa1b40b67065855a8b31d19d8c
woophi <attendentofsky@gmail.com>
2022-09-14 11:26:25 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@ef0a2bc58ace7db5b835a0f87d097d8edc835e58 🚀

---
+++ assets/IconButton.stories.09310bbd.js
@@ -0,0 +1,36 @@
+import{I as t}from"./IconButton.36a4f7eb.js";import{s as r}from"./CalendarEventIcon.ab884651.js";import{j as n}from"./jsx-runtime.923e9fc2.js";import"./clsx.3b79bc83.js";import"./Spinner.6d1fbe9a.js";/* empty css                             */import"./vanilla-extract-recipes-createRuntimeFn.esm.36bbe845.js";import"./iframe.3d3ff981.js";const B={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { IconButton } from '../components/icon-button';
+import { TrophyIcon } from '../icons';
+
+export default {
+  title: 'UI/Icon Button',
+  component: IconButton,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
+} as ComponentMeta<typeof IconButton>;
+
+const Template: ComponentStory<typeof IconButton> = args => <IconButton {...args} children={<TrophyIcon />} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: 'secondary',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
+`,locationsMap:{primary:{startLoc:{col:52,line:15},endLoc:{col:110,line:15},startBody:{col:52,line:15},endBody:{col:110,line:15}},secondary:{startLoc:{col:52,line:15},endLoc:{col:110,line:15},startBody:{col:52,line:15},endBody:{col:110,line:15}},disabled:{startLoc:{col:52,line:15},endLoc:{col:110,line:15},startBody:{col:52,line:15},endBody:{col:110,line:15}},loading:{startLoc:{col:52,line:15},endLoc:{col:110,line:15},startBody:{col:52,line:15},endBody:{col:110,line:15}}}}},title:"UI/Icon Button",component:t,argTypes:{disabled:{type:"boolean"}}},o=e=>n(t,{...e,children:n(r,{})}),a=o.bind({});a.args={color:"primary"};const c=o.bind({});c.args={color:"secondary"};const i=o.bind({});i.args={disabled:!0};const l=o.bind({});l.args={loading:!0};const I=["Primary","Secondary","Disabled","Loading"];export{i as Disabled,l as Loading,a as Primary,c as Secondary,I as __namedExportsOrder,B as default};
+//# sourceMappingURL=IconButton.stories.09310bbd.js.map
---
+++ assets/preview.bb792076.js
@@ -0,0 +1,2 @@
+import{j as r}from"./jsx-runtime.923e9fc2.js";/* empty css                             */import"./iframe.3d3ff981.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
+//# sourceMappingURL=preview.bb792076.js.map
