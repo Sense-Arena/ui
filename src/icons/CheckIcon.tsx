38ae1da3cd67e80fbefdf458fbac356929689ce1
Konstantin <konstantin.miheev@sensearena.com>
2022-09-14 16:37:38 +0200

    added CheckIcon

---
+++ src/icons/CheckIcon.tsx
@@ -0,0 +1,21 @@
+import { DetailedHTMLProps, forwardRef, SVGAttributes } from 'react';
+
+type Props = { className?: string } & DetailedHTMLProps<SVGAttributes<SVGSVGElement>, SVGSVGElement>;
+export const CheckIcon = forwardRef<SVGSVGElement, Props>(({ className, ...rest }, ref) => {
+  return (
+    <svg
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      ref={ref}
+      {...rest}
+    >
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z" />
+      </g>
+    </svg>
+  );
+});
