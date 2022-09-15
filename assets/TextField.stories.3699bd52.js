a05f15f288a865853cc7564183de161ee10353d1
Denior1 <36446798+Denior1@users.noreply.github.com>
2022-09-15 07:22:51 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@89da7ffec713c7f8fa9573e864f45c14127bcdac 🚀

---
+++ assets/TextField.stories.3699bd52.js
@@ -0,0 +1,70 @@
+import{a as o}from"./TextField.8e3854c4.js";import{I as l}from"./IconButton.abeff799.js";import{S as t}from"./YoutubeIcon.bb6c412f.js";import{j as n}from"./jsx-runtime.559ba58b.js";import"./clsx.3b79bc83.js";import"./Paragraph.8bbc9226.js";/* empty css                             */import"./vanilla-extract-recipes-createRuntimeFn.esm.36bbe845.js";import"./styled.a12b28b4.js";import"./TransitionGroupContext.690eae39.js";import"./Spinner.8bfbc78e.js";import"./iframe.589ee44a.js";const h={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { TextField } from '../../components/inputs';
+import { IconButton } from '../../components/icon-button';
+import { SearchIcon } from '../../icons';
+
+export default {
+  title: 'UI/Inputs/TextField',
+  component: TextField,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
+} as ComponentMeta<typeof TextField>;
+
+const Template: ComponentStory<typeof TextField> = args => <TextField {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+  label: 'First Name',
+  name: 'first_name',
+  placeholder: 'write',
+};
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  label: 'First Name',
+  name: 'first_name',
+  endAdornment: <SearchIcon />,
+};
+
+export const WithIconButton = Template.bind({});
+WithIconButton.args = {
+  label: 'First Name',
+  name: 'first_name',
+  endAdornment: (
+    <IconButton>
+      <SearchIcon />
+    </IconButton>
+  ),
+};
+
+export const Filled = Template.bind({});
+Filled.args = {
+  label: 'First Name',
+  name: 'first_name',
+  value: 'Konstantin',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'First Name',
+  name: 'first_name',
+  disabled: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  label: 'Last Name',
+  name: 'last_name',
+  errorText: 'Error message lorem ipsum',
+};
+export const ErrorFilled = Template.bind({});
+ErrorFilled.args = {
+  label: 'Last Name',
+  name: 'last_name',
+  value: 'AAAAAAAA',
+  errorText: 'Error message lorem ipsum',
+};
+`,locationsMap:{base:{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},"with-icon":{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},"with-icon-button":{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},filled:{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},disabled:{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},error:{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}},"error-filled":{startLoc:{col:51,line:16},endLoc:{col:82,line:16},startBody:{col:51,line:16},endBody:{col:82,line:16}}}}},title:"UI/Inputs/TextField",component:o,argTypes:{disabled:{type:"boolean"}}},e=r=>n(o,{...r}),a=e.bind({});a.args={label:"First Name",name:"first_name",placeholder:"write"};const i=e.bind({});i.args={label:"First Name",name:"first_name",endAdornment:n(t,{})};const s=e.bind({});s.args={label:"First Name",name:"first_name",endAdornment:n(l,{children:n(t,{})})};const m=e.bind({});m.args={label:"First Name",name:"first_name",value:"Konstantin"};const c=e.bind({});c.args={label:"First Name",name:"first_name",disabled:!0};const d=e.bind({});d.args={label:"Last Name",name:"last_name",errorText:"Error message lorem ipsum"};const p=e.bind({});p.args={label:"Last Name",name:"last_name",value:"AAAAAAAA",errorText:"Error message lorem ipsum"};const _=["Base","WithIcon","WithIconButton","Filled","Disabled","Error","ErrorFilled"];export{a as Base,c as Disabled,d as Error,p as ErrorFilled,m as Filled,i as WithIcon,s as WithIconButton,_ as __namedExportsOrder,h as default};
+//# sourceMappingURL=TextField.stories.3699bd52.js.map
