7a0347a74dbf308229addeac29bd1f5fa5a93725
Denior1 <36446798+Denior1@users.noreply.github.com>
2022-09-15 08:25:50 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@34b8ae15c9ecf1d4827eb6a568d0b8e5871247cd 🚀

--- assets/preview.4f0e2a51.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./jsx-runtime.559ba58b.js";/* empty css                             */import"./iframe.589ee44a.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
-//# sourceMappingURL=preview.4f0e2a51.js.map
