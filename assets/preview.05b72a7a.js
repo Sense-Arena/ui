ad97351f71599eaa639f4dd5cb7bdd903d9202b7
denis-savelev <36446798+denis-savelev@users.noreply.github.com>
2022-09-16 10:29:59 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@6336f76cf42aedeae96d80ac74cfc28c0d07066a 🚀

---
+++ assets/preview.05b72a7a.js
@@ -0,0 +1,2 @@
+import{j as r}from"./jsx-runtime.29e4528a.js";/* empty css                             */import"./iframe.192ac6bd.js";var a="_16vbc6o0";const c=[o=>r("div",{className:a,children:o()})],m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{c as decorators,m as parameters};
+//# sourceMappingURL=preview.05b72a7a.js.map
