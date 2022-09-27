3933882e0e2b0090376668b2f07df12c14716b28
Konstantin <konstantin.miheev@sensearena.com>
2022-09-27 13:30:09 +0200

    add more colors

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -1,4 +1,4 @@
-import { disableSelectStyle, linkInsertion, redTitle, subtitleSection, vars } from './theme.css';
+import { disableSelectStyle, linkInsertion, redTitle, subtitleSection, textNoWrap, vars } from './theme.css';
 
 /**
  * production only
@@ -12,4 +12,5 @@ export const themeStyles = {
   linkInsertion,
   redTitle,
   subtitleSection,
+  textNoWrap,
 };
