5975728216cea0bb1c8ce39082f9874f55417694
Konstantin <konstantin.miheev@sensearena.com>
2022-10-04 15:05:29 +0200

    fix css vars

--- src/theme/theme.css.ts
+++ src/theme/theme.css.ts
@@ -1,37 +1,5 @@
-import { createGlobalTheme, globalStyle } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 import { linkInsertion, vars } from './vars.css';
-
-createGlobalTheme('body', vars, {
-  colors: {
-    red: '#E4022D',
-    redShade: 'rgba(228,2,45,0.15)',
-    redDarken: '#D30028',
-    green: '#1A994A',
-    greenShade: 'rgba(26,153,74,0.15)',
-    white: '#ffffff',
-    black: '#000000',
-    grey: '#D7D9DF',
-    greyDark: '#E3E6EE',
-    greyLight: '#F6F8FD',
-    greyLight2: '#ECEFF9',
-    blackMetal: '#242424',
-    blackMetalShade: 'rgba(36,36,36,0.15)',
-    text: '#1C1C1C',
-    ctaDark: '#292929',
-    logoBlack: '#1c1d1c',
-  },
-  font: {
-    family: 'Rubik, sans-serif',
-  },
-  zIndex: {
-    menu: '11000',
-    menuSide: '11001',
-    playerOverlay: '10999',
-    modal: '10998',
-    gdpr: '10997',
-    dropdown: '9999',
-  },
-});
 
 globalStyle('body', {
   fontFamily: vars.font.family,
