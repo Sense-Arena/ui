6ff2edfa922f2ecc398e3e46e097e35c76da8fcf
Denis Savelev <denis.savelev@sensearena.com>
2022-10-05 15:10:42 +0400

    add portal in modal

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -16,6 +16,7 @@ const Template: ComponentStory<typeof Modal> = args => {
     <>
       <Button onClick={() => setOpen(true)}>Open modal</Button>
       <Modal {...args} open={open} handleClose={() => setOpen(false)} />
+      <div style={{ width: 400, height: 400, position: 'relative' }} id="customPortal" />
     </>
   );
 };
@@ -190,4 +191,12 @@ WithoutCard.args = {
 export const UpdateModalContent = TemplateUpdateModalContent.bind({});
 UpdateModalContent.args = {
   title: 'Modal',
+};
+
+export const CustomPortal = Template.bind({});
+CustomPortal.args = {
+  title: 'Modal',
+  portalNodeId: 'customPortal',
+  children:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 };
