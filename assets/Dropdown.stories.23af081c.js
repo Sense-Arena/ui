de544cb4ee6af8a4c7cc9e3ff38c5e9e421c2e7e
denis-savelev <36446798+denis-savelev@users.noreply.github.com>
2022-10-11 14:35:36 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@7859ea97e6e74481d643949797ed03779fab5997 🚀

---
+++ assets/Dropdown.stories.23af081c.js
@@ -0,0 +1,106 @@
+import{r as l,a as H,j as t}from"./jsx-runtime.b924709e.js";import{u as w,a as D,D as x}from"./DropdownMenu.474697fd.js";import{h as B,a as T}from"./YoutubeIcon.a8aa552b.js";import{c as V}from"./clsx.3b79bc83.js";/* empty css                            */import{c as h}from"./vanilla-extract-recipes-createRuntimeFn.esm.57f0c9b7.js";import"./iframe.6abb3a8e.js";import"./react-spring-web.esm.e84c352b.js";import"./index.a073d083.js";var C=h({defaultClassName:"_16ov49e0",variantClassNames:{border:{black:"_16ov49e1",grey:"_16ov49e2"},disabled:{true:"_16ov49e3"},error:{true:"_16ov49e4"},opened:{true:"_16ov49e5"},fullWidth:{true:"_16ov49e6"},size:{s:"_16ov49e7",m:"_16ov49e8",l:"_16ov49e9"},bRadius:{8:"_16ov49ea",10:"_16ov49eb"}},defaultVariants:{size:"m",bRadius:10},compoundVariants:[]}),y="_16ov49ed",R="_16ov49ec";function s({disabled:o,onSelect:r,selectedOptionLabel:f,options:g,error:S,className:v,selectedOption:k,border:O,fullWidth:L,size:p,bRadius:u}){const[a,m]=l.exports.useState(!1),b=l.exports.useRef(null),d=l.exports.useRef(null),A=l.exports.useCallback(()=>{o||m(c=>!c)},[o]),i=l.exports.useCallback(()=>m(!1),[]);w("keyup",i,"Escape"),D(b,i,d);const _=l.exports.useCallback(c=>{r==null||r(c),i()},[r]);return H("div",{onClick:A,className:V(C({disabled:o,error:S,opened:a,border:O,fullWidth:L,size:p,bRadius:u}),v),ref:d,children:[t("span",{className:R,children:f}),t("div",{children:a?t(B,{className:y}):t(T,{className:y})}),t(x,{menuRef:b,isOpen:a,mainRef:d,selectedOption:k,selectItem:_,options:g,size:p,bRadius:u})]})}try{s.displayName="Dropdown",s.__docgenInfo={description:"",displayName:"Dropdown",props:{disabled:{defaultValue:null,description:"",name:"disabled",required:!1,type:{name:"boolean | undefined"}},error:{defaultValue:null,description:"",name:"error",required:!1,type:{name:"boolean | undefined"}},onSelect:{defaultValue:null,description:"",name:"onSelect",required:!1,type:{name:"((selected: TOption) => void) | undefined"}},selectedOptionLabel:{defaultValue:null,description:"",name:"selectedOptionLabel",required:!0,type:{name:"ReactNode"}},selectedOption:{defaultValue:null,description:"",name:"selectedOption",required:!0,type:{name:"TOption"}},options:{defaultValue:null,description:"",name:"options",required:!0,type:{name:"{ title: string; value: TOption; }[]"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string | undefined"}},border:{defaultValue:null,description:"",name:"border",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"black"'},{value:'"grey"'}]}},fullWidth:{defaultValue:null,description:"",name:"fullWidth",required:!1,type:{name:"boolean | undefined"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"s"'},{value:'"m"'},{value:'"l"'}]}},bRadius:{defaultValue:null,description:"",name:"bRadius",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:"8"},{value:"10"}]}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/dropdown/Dropdown.tsx#Dropdown"]={docgenInfo:s.__docgenInfo,name:"Dropdown",path:"src/components/dropdown/Dropdown.tsx#Dropdown"})}catch{}const X={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Dropdown } from '../components/dropdown';
+
+export default {
+  title: 'UI/Dropdown',
+  component: Dropdown,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
+} as ComponentMeta<typeof Dropdown>;
+
+const options = [
+  {
+    title: '1',
+    value: '1',
+  },
+  {
+    title: '2',
+    value: '2',
+  },
+  {
+    title: '3',
+    value: '3',
+  },
+  {
+    title: '4',
+    value: '4',
+  },
+  {
+    title: '5',
+    value: '5',
+  },
+];
+
+const Template: ComponentStory<typeof Dropdown> = args => <Dropdown {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+  selectedOption: '1',
+  selectedOptionLabel: '1',
+  onSelect: console.debug,
+  options,
+  border: 'grey',
+};
+export const LongValue = Template.bind({});
+LongValue.args = {
+  selectedOption: '1',
+  selectedOptionLabel:
+    'Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey',
+  onSelect: console.debug,
+  options,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  selectedOption: '1',
+  selectedOptionLabel: 'Sense Arena Hockey dsfsdkf ',
+  onSelect: console.debug,
+  options,
+  disabled: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  selectedOption: '1',
+  selectedOptionLabel: 'Sense Arena Hockey dsfsdkf ',
+  onSelect: console.debug,
+  options,
+  error: true,
+  border: 'grey',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  selectedOption: '1',
+  selectedOptionLabel: 'Sense Arena Hockey dsfsdkf ',
+  onSelect: console.debug,
+  options,
+  border: 'grey',
+  size: 's',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  selectedOption: '1',
+  selectedOptionLabel: 'Sense Arena Hockey dsfsdkf ',
+  onSelect: console.debug,
+  options,
+  border: 'grey',
+  size: 'l',
+};
+
+export const FullWidth = Template.bind({});
+FullWidth.args = {
+  selectedOption: '1',
+  selectedOptionLabel: 'Sense Arena Hockey dsfsdkf ',
+  onSelect: console.debug,
+  options,
+  border: 'grey',
+  size: 'l',
+  fullWidth: true,
+};
+`,locationsMap:{base:{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},"long-value":{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},disabled:{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},error:{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},small:{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},large:{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}},"full-width":{startLoc:{col:50,line:37},endLoc:{col:80,line:37},startBody:{col:50,line:37},endBody:{col:80,line:37}}}}},title:"UI/Dropdown",component:s,argTypes:{disabled:{type:"boolean"}}},e=[{title:"1",value:"1"},{title:"2",value:"2"},{title:"3",value:"3"},{title:"4",value:"4"},{title:"5",value:"5"}],n=o=>t(s,{...o}),q=n.bind({});q.args={selectedOption:"1",selectedOptionLabel:"1",onSelect:console.debug,options:e,border:"grey"};const E=n.bind({});E.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey Sense Arena Hockey",onSelect:console.debug,options:e};const N=n.bind({});N.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey dsfsdkf ",onSelect:console.debug,options:e,disabled:!0};const z=n.bind({});z.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey dsfsdkf ",onSelect:console.debug,options:e,error:!0,border:"grey"};const W=n.bind({});W.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey dsfsdkf ",onSelect:console.debug,options:e,border:"grey",size:"s"};const I=n.bind({});I.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey dsfsdkf ",onSelect:console.debug,options:e,border:"grey",size:"l"};const F=n.bind({});F.args={selectedOption:"1",selectedOptionLabel:"Sense Arena Hockey dsfsdkf ",onSelect:console.debug,options:e,border:"grey",size:"l",fullWidth:!0};const Z=["Base","LongValue","Disabled","Error","Small","Large","FullWidth"];export{q as Base,N as Disabled,z as Error,F as FullWidth,I as Large,E as LongValue,W as Small,Z as __namedExportsOrder,X as default};
+//# sourceMappingURL=Dropdown.stories.23af081c.js.map
---
+++ assets/es.map.constructor.1ebef043.js
@@ -0,0 +1,2 @@
+import{n as o,o as t}from"./iframe.6abb3a8e.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
+//# sourceMappingURL=es.map.constructor.1ebef043.js.map
