47d5c479db61c6b2f4cd93356154782efc1a743d
Denis Savelev <denis.savelev@sensearena.com>
2022-10-27 17:34:04 +0400

    add onClick, hover effect for Table Row

--- src/components/table/TRow.tsx
+++ src/components/table/TRow.tsx
@@ -2,11 +2,11 @@ import React, { PropsWithChildren } from 'react';
 import { tableStyles } from './table.css';
 import { clsx } from '../../utils';
 
-type Props = PropsWithChildren<{ className?: string }>;
+type Props = PropsWithChildren<{ className?: string; onClick?: () => void }>;
 
-export const TRow = ({ className, children, ...rest }: Props) => {
+export const TRow = ({ className, children, onClick, ...rest }: Props) => {
   return (
-    <tr {...rest} className={clsx(className)}>
+    <tr {...rest} className={clsx(className, tableStyles.withHover({ hover: !!onClick }))} onClick={onClick}>
       {children}
     </tr>
   );
