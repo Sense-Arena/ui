a870ccb8af01bdb6055ca96e21fdff362f6f629e
Konstantin <konstantin.miheev@sensearena.com>
2022-12-15 09:57:24 +0100

    up deps

---
+++ vite.test.ts
@@ -0,0 +1,47 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { peerDependencies } from './package.json';
+
+export default defineConfig({
+  plugins: [
+    vanillaExtractPlugin(),
+    react({
+      jsxRuntime: 'classic',
+    }),
+    dts({
+      outputDir: path.resolve(__dirname, 'dist/types'),
+      exclude: ['**/stories/**'],
+      entryRoot: path.resolve(__dirname, 'src/index.ts'),
+      afterDiagnostic: console.debug,
+      skipDiagnostics: false,
+      copyDtsFiles: true,
+    }),
+    visualizer({
+      emitFile: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'UI',
+      formats: ['es', 'umd'],
+      fileName: format => `@sa-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      output: {
+        globals: {
+          '@react-spring/web': 'SpringWeb',
+          '@vanilla-extract/css': 'VanillaCss',
+          '@vanilla-extract/recipes': 'VanillaRecipes',
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
