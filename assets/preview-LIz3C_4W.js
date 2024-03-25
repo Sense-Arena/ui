08ce57005b336a86ae0fd30accd71e015a0c9118
woophi <attendentofsky@gmail.com>
2024-03-25 09:01:23 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@c31088882e1314d133595fe9bc34659bb197a80a 🚀

---
+++ assets/preview-LIz3C_4W.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./iframe-1_5eVQLT.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-uLefv2q1.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-LIz3C_4W.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-uLefv2q1.js","./iframe-1_5eVQLT.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-63cjBzeA.js","./index-aKxH7ZY0.js","./index-KSw0QYaF.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
