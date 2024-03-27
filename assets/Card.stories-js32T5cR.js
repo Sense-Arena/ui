5a7bcc63b8e9ba99acd701272fdf37627d1a3e14
woophi <attendentofsky@gmail.com>
2024-03-27 09:25:12 +0000

    Deploying to gh-pages from @ Sense-Arena/ui@d3e37aae15eb5a8aea9b61ce0ccc7ad5e6135e8c 🚀

---
+++ assets/Card.stories-js32T5cR.js
@@ -0,0 +1,46 @@
+import{R as e}from"./index-4g5l5LRQ.js";import{C}from"./Card-FkLLeLMB.js";import{B as t}from"./Button-yyQ_Li1O.js";import{P as o}from"./Paragraph-auBEudWP.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./Divider-GtSk6P8B.js";/* empty css                            */import"./createRuntimeFn-f8e161c6.esm-AdCGDnTf.js";import"./clsx-4u0nZvpz.js";import"./Heading-S1gG8C8U.js";import"./typography.style.css-PSZ9VnJU.js";import"./Spinner-xQQlIf9B.js";const L={title:"UI/Card",component:C,tags:["autodocs"],argTypes:{withPadding:{type:"boolean"},className:{type:"string"},title:{type:"string"}}},s=d=>e.createElement("div",{style:{position:"relative",backgroundColor:"#eceff9",height:"100vh",padding:24}},e.createElement(C,{withPadding:d.withPadding,title:d.title,action:d.action},d.children)),i=s.bind({});i.args={title:"Title",action:e.createElement(t,{minWidth:!0,mode:"square",color:"outline_secondary"},"Action"),children:e.createElement(e.Fragment,null,e.createElement(o,{variant:"caption"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),e.createElement(t,{style:{marginTop:12}},"Test"))};const a=s.bind({});a.args={title:e.createElement(o,{variant:"note"},"Title"),action:e.createElement(t,{minWidth:!0,mode:"square",color:"outline_secondary"},"Action"),children:e.createElement(e.Fragment,null,e.createElement(o,{variant:"caption"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),e.createElement(t,{style:{marginTop:12}},"Test"))};const n=s.bind({});n.args={title:void 0,action:void 0,children:e.createElement(e.Fragment,null,e.createElement(o,{variant:"caption"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),e.createElement(t,{style:{marginTop:12}},"Test"))};const r=s.bind({});r.args={title:void 0,action:void 0,withPadding:!1,children:e.createElement(e.Fragment,null,e.createElement(o,{variant:"caption"},"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),e.createElement(t,{style:{marginTop:12}},"Test"))};var l,c,u;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`args => {
+  return <div style={{
+    position: 'relative',
+    backgroundColor: '#eceff9',
+    height: '100vh',
+    padding: 24
+  }}>
+      <Card withPadding={args.withPadding} title={args.title} action={args.action}>
+        {args.children}
+      </Card>
+    </div>;
+}`,...(u=(c=i.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var m,p,g;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  return <div style={{
+    position: 'relative',
+    backgroundColor: '#eceff9',
+    height: '100vh',
+    padding: 24
+  }}>
+      <Card withPadding={args.withPadding} title={args.title} action={args.action}>
+        {args.children}
+      </Card>
+    </div>;
+}`,...(g=(p=a.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var h,v,f;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`args => {
+  return <div style={{
+    position: 'relative',
+    backgroundColor: '#eceff9',
+    height: '100vh',
+    padding: 24
+  }}>
+      <Card withPadding={args.withPadding} title={args.title} action={args.action}>
+        {args.children}
+      </Card>
+    </div>;
+}`,...(f=(v=n.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};var b,q,E;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  return <div style={{
+    position: 'relative',
+    backgroundColor: '#eceff9',
+    height: '100vh',
+    padding: 24
+  }}>
+      <Card withPadding={args.withPadding} title={args.title} action={args.action}>
+        {args.children}
+      </Card>
+    </div>;
+}`,...(E=(q=r.parameters)==null?void 0:q.docs)==null?void 0:E.source}}};const R=["Base","TitleIsReactNode","WithoutTitleAndAction","WithoutPadding"];export{i as Base,a as TitleIsReactNode,r as WithoutPadding,n as WithoutTitleAndAction,R as __namedExportsOrder,L as default};
+//# sourceMappingURL=Card.stories-js32T5cR.js.map
--- assets/syntaxhighlighter-NMPM6SWI-bhC6DxIh.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./index-QWa2NkTP.js";import"./iframe-h6sfGylj.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./index-aKxH7ZY0.js";import"./index-KSw0QYaF.js";import"./index-PPLHz8o0.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-bhC6DxIh.js.map
